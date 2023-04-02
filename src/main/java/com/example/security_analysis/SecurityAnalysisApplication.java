f31c0bc459848a6fcf9a0a5dc92d1f6a04c8b045
szy <2908764770@qq.com>
2023-04-02 14:35:51 +0800

    初始化项目

---
+++ src/main/java/com/example/security_analysis/SecurityAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.example.security_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/security_analysis/SecurityAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
